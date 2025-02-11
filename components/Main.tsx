ee038b2f2b0ff3c2e4ed581c74c6bd622b3a7d31
Muhammad Ardiansyah <ardi722308@gmail.com>
2025-02-11 20:42:04 +0800

    update

--- components/Main.tsx
+++ components/Main.tsx
@@ -4,12 +4,12 @@ const Main = () => {
   return (
     <main className="flex flex-col items-center justify-center text-white  h-screen text-center p-4">
       <div className="relative">
-        <div className="w-40 h-40 bg-yellow-500 rounded-full blur-2xl absolute z-10"></div>
-        <Image src="/images/berandaHimakom.png" alt="HIMAKOM" className="w-40 h-40 relative z-20" />
+        <div className="w-64 h-64 bg-yellow-500 rounded-full blur-2xl absolute z-10"></div>
+        <Image src="/images/logoHimakom.png" alt="HIMAKOM" width={250} height={250} className="relative z-20" />
       </div>
-      <h1 className="text-3xl font-bold mt-4">HIMAKOM</h1>
-      <p className="text-lg">Himpunan Mahasiswa Ilmu Komputer</p>
-      <p className="text-sm text-gray-400">Universitas Mega Buana Palopo</p>
+      <h1 className="text-3xl font-bold mt-6">HIMAKOM</h1>
+      <p className="text-xl">Himpunan Mahasiswa Ilmu Komputer</p>
+      <p className="text-sm">Universitas Mega Buana Palopo</p>
     </main>
   );
 };
